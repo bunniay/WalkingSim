5e5ae84dfda8024f284e85184e69136c0f7bf5ac
bunniay <bunniaybb@gmail.com>
2026-02-03 11:39:15 -0500

    poopoo

---
+++ WalkingSimProject/Assets/Scripts/inclassscripts/ButtonTest.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public ScriptObjDemo demo;
+    public TMP_Text nameText;
+    public TMP_Text numberText;
+    public TMP_Text descriptionText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void GrabObjInfo(ScriptObjDemo demoInfo)
+    {
+        //nameText.text = demo.itemName;
+        //numbertext.text = demo.itemNumber.ToString();
+        //descriptionText.text = demo.itemDescription;
+        //demo.AssignInfo(nameText.text, numberText.text, descriptionText.text);
+        
+    }
+}
---
+++ WalkingSimProject/Assets/Scripts/inclassscripts/ScriptObjDemo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptObjDemo", menuName = "Scriptable Objects/ScriptObjDemo")]
+public class ScriptObjDemo : ScriptableObject
+{
+    public string itemName;
+    public string itemNumber;
+    [TextArea(4,4)]
+    public string itemDescription;
+    //public GameObject itemModel; 
+    public void AssignInfo(string nameString, string numberString, string itemString)
+    {
+        //nameString = itemName;
+        //numberString = itemNumber.ToString();
+        //itemString = itemDescription;
+        itemName = itemName;
+        itemNumber = itemNumber.ToString();
+        itemDescription = itemDescription;
+    }
+}
